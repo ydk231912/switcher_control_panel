0de4a5e3086c30fb637b408422cf680824d18eef
hzseeder <hzseeder>
2024-08-27 17:23:17 +0800

    删除无用依赖

--- thirdparty/stb/src/stb_image_impl.c
+++
@@ -1,9 +0,0 @@
-#ifndef STB_IMAGE_IMPLEMENTATION
-#define STB_IMAGE_IMPLEMENTATION
-#endif
-#include "stb_image.h"
-
-#ifndef STB_IMAGE_WRITE_IMPLEMENTATION
-#define STB_IMAGE_WRITE_IMPLEMENTATION
-#endif
-#include "stb_image_write.h"
